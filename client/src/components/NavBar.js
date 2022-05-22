4e97cf9f98e9ffc427ea876868410bd6ccf20306
Mark Momot <markmomot03@gmail.com>
2022-05-22 17:43:39 -0400

    Updated styles for navbar slightly

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import "./css/nav.css"
 
 function NavBar({user}){
@@ -6,10 +6,10 @@ function NavBar({user}){
     function logoutDropdown(){
         return (
             <div className="m-auto dropdown">
-                <button class="btn btn-dark dropdown-toggle text-capitalize btn-lg link-margin" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{user.name}</button>
-                <div class="dropdown-menu" aria-labelledBy="dropdownMenuButton">
-                    <Link to={`/${user.username}/profile`} className="dropdown-item">My Profile</Link>
-                    <Link to="/logout" className="dropdown-item">Logout</Link>
+                <button class="btn btn-dark dropdown-toggle text-capitalize btn-lg" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{user.name}</button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <NavLink to={`/${user.username}/profile`} className="dropdown-item">My Profile</NavLink>
+                    <NavLink to="/logout" className="dropdown-item">Logout</NavLink>
                 </div>
             </div>
         )
@@ -17,10 +17,10 @@ function NavBar({user}){
 
     return (
         <nav className="nav justify-content-center nav-tabs">
-            <Link to="/" className="btn btn-dark m-auto btn-lg">Home</Link>
-            <Link to="/about" className="btn btn-dark m-auto btn-lg">About Me</Link>
-            <Link to="/contact" className="btn btn-dark m-auto btn-lg">Contact Me</Link>
-            {!user ? <Link to="/login" className="btn btn-dark m-auto btn-lg">Login</Link> : logoutDropdown()}
+            <NavLink exact to="/" className="btn btn-dark m-auto btn-lg">Home</NavLink>
+            <NavLink to="/about" className="btn btn-dark m-auto btn-lg">About Me</NavLink>
+            <NavLink to="/contact" className="btn btn-dark m-auto btn-lg">Contact Me</NavLink>
+            {!user ? <NavLink to="/login" className="btn btn-dark m-auto btn-lg">Login</NavLink> : logoutDropdown()}
         </nav>
     )
 }
