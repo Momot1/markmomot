e8267b6988b613cc28b9f06a31e32490745d11b7
Mark Momot <markmomot03@gmail.com>
2022-05-22 12:44:07 -0400

    Allow non logged in users to submit contact form

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -1,4 +1,6 @@
 class ContactFormController < ApplicationController
+    skip_before_action :authorized, only: :create
+
     def create
         c = ContactForm.new(name: params[:name], email: params[:email], message: params[:message])
         c.deliver
