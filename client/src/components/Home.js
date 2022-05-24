89a2d066988cc7e80f8c352021cc562f3056a4cf
Mark Momot <markmomot03@gmail.com>
2022-05-24 16:06:04 -0400

    Updated homepage slightly

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,21 @@
 function Home(){
     return (
-        <div className="justify-content-center">
-           <h1>YOOOOOOOOOOOOOOOOO</h1>
-           <h1>THIS IS MARK MOMOT'S WEBSITE</h1>
+        <div className="justify-content-center text-center">
+           <h1 className="border-bottom">Welcome to my webpage</h1>
+           <div className="border-bottom">
+               <h3>Who am I?</h3>
+           </div>
+           <div className="border-bottom">
+               <h3>What is this site about?</h3>
+           </div>
+           <div className="border-bottom">
+               <h3>My experiences as a developer</h3>
+           </div>
+           <footer>
+                <h6>Feel free to reach out to me by submitting the contact me form.</h6>
+
+                <a href="#top">Back to top</a>
+           </footer>
         </div>
     )
 }
