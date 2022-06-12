b8480f3865f22ee788f12feb7bdf5d7bd6b31028
Mark Momot <markmomot03@gmail.com>
2022-06-12 19:56:12 -0400

    Added margin to the bottom of the footer

--- client/src/components/MainFooter.js
+++ client/src/components/MainFooter.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./css/footer.css";
 
 export default function MainFooter() {
   return (
-    <div className="justify-content-center text-center">
+    <div className="justify-content-center text-center" id="footer">
       <h6>
         Feel free to reach out to me by submitting the{" "}
         <Link to="/contact">contact me</Link> form.
