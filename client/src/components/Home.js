7e9006532bf1307db316197dc06411d91cc947e8
Mark Momot <markmomot03@gmail.com>
2022-06-12 21:05:44 -0400

    Updated home and about me page content

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,7 +8,19 @@ function Home() {
       <h1 id="home-title">Welcome to my webpage</h1>
       <div className="border-bottom">
         <h3 id="home-h3">What is this site about?</h3>
-        <p>{"{placeholder}"}</p>
+        <p>
+          This website is meant to demonstrate my abilities as a software
+          developer. You can read all about me by going to the{" "}
+          <Link to="/about">about me</Link> page. But to give a quick summary
+          about myself, over the past 40 weeks or so, I have been learning how
+          to develop full stack web applications.
+        </p>
+        <h4>
+          DISCLAIMER: This website is still under development. I am currently in
+          school, so I spend my Sunday's working on my personal website, and
+          Monday-Saturday working through school. As a result, there might be
+          some bugs and some incompleted features and pages.
+        </h4>
       </div>
       {window.innerHeight < 208 ? <MainFooter /> : null}
     </div>
