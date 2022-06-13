3a677126afb12bbed6dd557f5d69f117059e5e4c
Mark Momot <markmomot03@gmail.com>
2022-06-12 21:41:44 -0400

    Updated about me page styling

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,20 +1,21 @@
-function Profile({user}) {
-    if(!user){
-        return (
-            <>Loading</>
-        )
-    }
+function Profile({ user }) {
+  if (!user) {
+    return <>Loading</>;
+  }
 
-
-    return (
-        <div>
-            <h1>Profile for {user.name}</h1>
-            <h3>Username: {user.username}</h3>
-            <h3>Email: {user.email}</h3>
-            <h3>Birthday: {user.birthday}</h3>
-            {user.is_admin ? <h3>Account Type: Admin</h3>: <h3>Account Type: Member</h3>}
-        </div>
-    )
+  return (
+    <div>
+      <h1>Profile for {user.name}</h1>
+      <h3>Username: {user.username}</h3>
+      <h3>Email: {user.email}</h3>
+      <h3>Birthday: {user.birthday}</h3>
+      {user.is_admin ? (
+        <h3>Account Type: Administrator</h3>
+      ) : (
+        <h3>Account Type: Member</h3>
+      )}
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
